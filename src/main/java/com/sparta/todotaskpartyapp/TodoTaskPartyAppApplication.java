f0775adebcb513e2583300269a0ffaf135cebda1
ParkDongSeob <bagd0445@gmail.com>
2024-03-11 20:24:32 +0900

    init commit

---
+++ src/main/java/com/sparta/todotaskpartyapp/TodoTaskPartyAppApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todotaskpartyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTaskPartyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoTaskPartyAppApplication.class, args);
+    }
+
+}
