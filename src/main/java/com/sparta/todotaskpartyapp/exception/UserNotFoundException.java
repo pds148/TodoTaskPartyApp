9db603ab30c0a473e10d3a1dabea81b9bd91cc1d
ParkDongSeob <bagd0445@gmail.com>
2024-03-13 17:30:55 +0900

    UserNotFoundException 코드 수정

--- src/main/java/com/sparta/todotaskpartyapp/exception/UserNotFoundException.java
+++ src/main/java/com/sparta/todotaskpartyapp/exception/UserNotFoundException.java
@@ -6,22 +6,7 @@ import java.io.Serial;
 
 public class UserNotFoundException extends RuntimeException {
 
-    public UserNotFoundException() {
-    }
-
     public UserNotFoundException(String message) {
         super(message);
-    }
-
-    public UserNotFoundException(String message, Throwable status) {
-        super(message, status);
-    }
-
-    public UserNotFoundException(Throwable status) {
-        super(status);
-    }
-
-    public UserNotFoundException(String message, Throwable status, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, status, enableSuppression, writableStackTrace);
     }
 }
