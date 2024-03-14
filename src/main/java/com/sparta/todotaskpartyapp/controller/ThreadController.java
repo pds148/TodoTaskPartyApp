44b9639f85225de97e82c2fbf746919b39ffd541
ParkDongSeob <bagd0445@gmail.com>
2024-03-14 18:03:30 +0900

    ThreadController 코드 수정

--- src/main/java/com/sparta/todotaskpartyapp/controller/ThreadController.java
+++ src/main/java/com/sparta/todotaskpartyapp/controller/ThreadController.java
@@ -1,6 +1,7 @@
 package com.sparta.todotaskpartyapp.controller;
 
 import com.sparta.todotaskpartyapp.dto.response.ThreadResponseDTO;
+import com.sparta.todotaskpartyapp.entity.Thread;
 import com.sparta.todotaskpartyapp.service.ThreadService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,7 @@ public class ThreadController {
     private final ThreadService threadService;
 
     @GetMapping("/search")
-    public List<ThreadResponseDTO> searchThreads(@RequestParam("threadId") Long threadId, @RequestParam("keyword") String keyword) {
+    public List<Thread> searchThreads(@RequestParam("threadId") Long threadId, @RequestParam("keyword") String keyword) {
         ThreadResponseDTO searchDTO = new ThreadResponseDTO(threadId, keyword);
         return threadService.searchThreads(searchDTO);
     }
