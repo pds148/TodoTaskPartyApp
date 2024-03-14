22cb02ed6152af14fe641185a7251134d686cf63
ParkDongSeob <bagd0445@gmail.com>
2024-03-14 10:21:18 +0900

    ThreadSearchResponseDTO 코드 수정

--- src/main/java/com/sparta/todotaskpartyapp/dto/response/ThreadSearchResponseDTO.java
+++ src/main/java/com/sparta/todotaskpartyapp/dto/response/ThreadSearchResponseDTO.java
@@ -8,13 +8,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ThreadResponseDTO {
+public class ThreadSearchResponseDTO {
     private Long id;
     private String content;
     private String createdAt;
     private String username;
 
-    public ThreadResponseDTO(Thread thread) {
+    public ThreadSearchResponseDTO(Thread thread) {
         this.id = thread.getId();
         this.content = thread.getContent();
         this.username = thread.getUsername();
